da7a4866e0465775ba7512cb0adafd997e537e3b
Ziya Genc <ziya.genc@uni.lu>
2018-11-01 07:22:18 +0100

    Commit first version.

---
+++ CryprenDecryptor/MainForm.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace CryprenDecryptor
+{
+    public partial class MainForm : Form
+    {
+        private List<string> encryptedFiles;
+        private static string password = "";
+        private static char[] alphabet =
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '!', '@', '#', '$', '%', '&', '*', '-', '=', '.', '+', '?'
+        };
+
+        private static int maxIndex = 7;
+        private static char[] guess = new char[8];
+        private static bool firstGuess = true;
+        private static byte[] buffer = new byte[16];
+        private static byte[] pdfSignature = { 0x25, 0x50, 0x44, 0x46 };
+        byte[] key;
+        byte[] iv;
+        static int counter = 0;
+        static Stopwatch sw = new Stopwatch();
+
+        #region FORM FUNCTIONS
+
+        public MainForm()
+        {
+            InitializeComponent();
+            encryptedFiles = new List<string>();
+            openFileDialog1.Filter = "Crypren Files (*.enc) | *.enc";
+        }
+
+        private void buttonAddFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                if (addFile(file))
+                {
+                    string logEntry = String.Format("{0}  Added file: {1}", DateTime.Now.ToString(), file);
+                    addLog(logEntry);
+                }
+            }
+        }
+
+        private void buttonAddFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string logEntry = String.Format("{0}  Scanning folder: {1}", DateTime.Now.ToString(), folderBrowserDialog1.SelectedPath);
+                addLog(logEntry);
+
+                try
+                {
+                    var files = GetFiles(folderBrowserDialog1.SelectedPath, "*.enc");
+
+                    foreach (string file in files)
+                    {
+                        addFile(file);
+                    }
+                }
+                catch
+                {
+                    logEntry = String.Format("{0}  Error occurred while scanning: {1}", DateTime.Now.ToString(), folderBrowserDialog1.SelectedPath);
+                    addLog(logEntry);
+                }
+
+                logEntry = String.Format("{0}  Finished scanning folder.", DateTime.Now.ToString());
+                addLog(logEntry);
+            }
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            string logEntry = String.Format("{0}  Scanning all drives for files: *.enc", DateTime.Now.ToString());
+            addLog(logEntry);
+
+            foreach (string letter in Directory.GetLogicalDrives())
+            {
+                var files = GetFiles(letter, ".enc");
+
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        string filenameWithEnc = file;
+                        addFile(filenameWithEnc);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error occurred while accessing: {0}", file);
+                        continue;
+                    }
+                }
+            }
+
+            logEntry = String.Format("{0}  Finished scanning.", DateTime.Now.ToString());
+            addLog(logEntry);
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            string logEntry;
+
+            if(encryptedFiles.Count == 0)
+            {
+                logEntry = String.Format("{0}  No files to decrypt. Did you perfom a scan or manually add a file?", DateTime.Now.ToString());
+                addLog(logEntry);
+                return;
+            }
+            
+            string candidateFile = GetCandidateFile();
+
+            if (candidateFile.Equals(String.Empty))
+            {
+                logEntry = String.Format("{0}  Could not find a candidate file.", DateTime.Now.ToString());
+                addLog(logEntry);
+
+                return;
+            }
+
+            logEntry = String.Format("{0}  Candidate file: {1}", DateTime.Now.ToString(), candidateFile);
+            addLog(logEntry);
+
+            try
+            {
+                using (FileStream fsCrypt = File.OpenRead(candidateFile))
+                {
+                    fsCrypt.Read(buffer, 0, 16);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "Unhandled Exception");
+                logEntry = String.Format("{0}  Exception occurred while reading candidate file.", DateTime.Now.ToString(), candidateFile);
+                addLog(logEntry);
+                return;
+            }
+
+            logEntry = String.Format("{0}  Started brute-force attack to find secret password.", DateTime.Now.ToString());
+            addLog(logEntry);
+
+            sw.Start();
+            BruteForce(0);
+
+            if (password == "")
+            {
+                logEntry = String.Format("{0}  Brute force finished. Could not find the password.", DateTime.Now.ToString());
+                addLog(logEntry);
+                return;
+            }
+
+            logEntry = String.Format("{0}  Found password: {1}", DateTime.Now.ToString(), password);
+            addLog(logEntry);
+
+            key = new UnicodeEncoding().GetBytes(password);
+            iv = new UnicodeEncoding().GetBytes(password);
+
+            logEntry = String.Format("{0}  Started decrypting files.", DateTime.Now.ToString());
+            addLog(logEntry);
+            logEntry = String.Format("{0}  Number of files: {1}", DateTime.Now.ToString(), encryptedFiles.Count);
+            addLog(logEntry);
+
+            foreach (var item in listBoxEncFiles.Items)
+            {
+                MemoryStream ms = new MemoryStream();
+                decryptFile(item.ToString());
+
+                if (checkBoxKeep.CheckState == CheckState.Unchecked)
+                {
+                    try
+                    {
+                        File.Delete(item.ToString());
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+            logEntry = String.Format("{0}  Finished decryption.", DateTime.Now.ToString());
+            addLog(logEntry);
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
+
+        private bool addFile(string file)
+        {
+            if (!encryptedFiles.Contains(file))
+            {
+                encryptedFiles.Add(file);
+                listBoxEncFiles.Items.Add(file);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void addLog(string logEntry)
+        {
+            listBoxLogs.Items.Add(logEntry);
+            listBoxLogs.SelectedIndex = listBoxLogs.Items.Count - 1;
+            listBoxLogs.SelectedIndex = -1;
+        }
+
+        // Taken from https://stackoverflow.com/a/4986333
+        internal static IEnumerable<string> GetFiles(string root, string searchPattern)
+        {
+            Stack<string> pending = new Stack<string>();
+            pending.Push(root);
+            while (pending.Count != 0)
+            {
+                var path = pending.Pop();
+                string[] next = null;
+                try
+                {
+                    next = Directory.GetFiles(path, searchPattern);
+                }
+                catch { }
+                if (next != null && next.Length != 0)
+                    foreach (var file in next) yield return file;
+                try
+                {
+                    next = Directory.GetDirectories(path);
+                    foreach (var subdir in next) pending.Push(subdir);
+                }
+                catch { }
+            }
+        }
+        
+        private string GetCandidateFile()
+        {
+            string candidateName = "";
+
+            foreach (var file in encryptedFiles)
+            {
+                string originalName = file.Remove(file.Length - 4, 4);
+                if (originalName.EndsWith(".pdf"))
+                {
+                    candidateName = file;
+                    break;
+                }
+            }
+
+            return candidateName;
+        }
+
+        private void BruteForce(int currentIndex)
+        {
+            for(int i1=0; i1<alphabet.Length; i1++)
+            {
+                guess[0] = alphabet[i1];
+
+                for (int i2 = 0; i2 < alphabet.Length; i2++)
+                {
+                    guess[1] = alphabet[i2];
+
+                    for (int i3 = 0; i3 < alphabet.Length; i3++)
+                    {
+                        guess[2] = alphabet[i3];
+
+                        for (int i4 = 0; i4 < alphabet.Length; i4++)
+                        {
+                            guess[3] = alphabet[i4];
+
+                            for (int i5 = 0; i5 < alphabet.Length; i5++)
+                            {
+                                guess[4] = alphabet[i5];
+
+                                for (int i6 = 0; i6 < alphabet.Length; i6++)
+                                {
+                                    guess[5] = alphabet[i6];
+
+                                    for (int i7 = 0; i7 < alphabet.Length; i7++)
+                                    {
+                                        guess[6] = alphabet[i7];
+
+                                        for (int i8 = 0; i8 < alphabet.Length; i8++)
+                                        {
+                                            guess[7] = alphabet[i8];
+
+                                            counter++;
+
+                                            if(counter == 1000000)
+                                            {
+                                                sw.Stop();
+                                                addLog(sw.Elapsed.TotalSeconds.ToString());
+                                                return;
+                                            }
+
+                                            byte[] key = new UnicodeEncoding().GetBytes(guess);
+                                            byte[] iv = new UnicodeEncoding().GetBytes(guess);
+
+                                            var rmCrypto = new RijndaelManaged();
+                                            rmCrypto.Padding = PaddingMode.Zeros;
+                                            var decryptor = rmCrypto.CreateDecryptor(key, iv);
+
+                                            using (MemoryStream msCrypt = new MemoryStream(buffer))
+                                            {
+                                                CryptoStream cs = new CryptoStream(msCrypt, decryptor, CryptoStreamMode.Read);
+                                                var decrypted = new MemoryStream();
+                                                cs.CopyTo(decrypted);
+
+                                                var decryptedArray = decrypted.ToArray();
+
+                                                bool result = true;
+
+                                                for (int j = 0; j < 4; j++)
+                                                    if (decryptedArray[j] != pdfSignature[j])
+                                                        result = false;
+
+                                                if (result)
+                                                {
+                                                    password = new string(guess);
+                                                    return;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void decryptFile(string encryptedFile)
+        {
+            try
+            {
+                using (FileStream fsCrypt = File.OpenRead(encryptedFile))
+                {
+                    using (MemoryStream msCrypt = new MemoryStream())
+                    {
+                        fsCrypt.CopyTo(msCrypt);
+                        string filenameWithoutEnc = Path.ChangeExtension(encryptedFile, null);
+                        using (FileStream fsDecrypted = File.Create(filenameWithoutEnc))
+                        {
+                            msCrypt.Position = 0;
+                            var msDecrypted = decryptStream(msCrypt, key, iv);
+                            msDecrypted.Position = 0;
+                            msDecrypted.CopyTo(fsDecrypted);
+
+                            string logEntry = String.Format("{0}  Decrypted file: {1}", DateTime.Now.ToString(), filenameWithoutEnc);
+                            addLog(logEntry);
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static MemoryStream decryptStream(MemoryStream ms, byte[] key, byte[] iv)
+        {
+            MemoryStream decrypted = null;
+
+            try
+            {
+                var rmCrypto = new RijndaelManaged();
+                var decryptor = rmCrypto.CreateDecryptor(key, iv);
+                CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                decrypted = new MemoryStream();
+                cs.CopyTo(decrypted);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return decrypted;
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            string message = "Decryptor for Crypren sample with SHA256: ce53233a435923a68a9ca6987f0d6333bb97d5a435b942d20944356ac29df598";
+
+            MessageBox.Show(message, "About");
+        }
+
+        private void checkBoxKeep_CheckStateChanged(object sender, EventArgs e)
+        {
+            string logEntry;
+
+            if (checkBoxKeep.CheckState == CheckState.Checked)
+            {
+                logEntry = String.Format("{0}  Encrypted files will be kept.", DateTime.Now.ToString());
+            }
+            else
+            {
+                logEntry = String.Format("{0}  Encrypted files will be deleted.", DateTime.Now.ToString());
+            }
+
+            addLog(logEntry);
+        }
+    }
+}
---
+++ CryprenDecryptor/MainForm.designer.cs
@@ -0,0 +1,213 @@
+﻿namespace CryprenDecryptor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelEncFiles = new System.Windows.Forms.Label();
+            this.listBoxEncFiles = new System.Windows.Forms.ListBox();
+            this.labelLogs = new System.Windows.Forms.Label();
+            this.listBoxLogs = new System.Windows.Forms.ListBox();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.buttonAddFile = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.checkBoxKeep = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonAddFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // labelEncFiles
+            // 
+            this.labelEncFiles.AutoSize = true;
+            this.labelEncFiles.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEncFiles.Location = new System.Drawing.Point(13, 9);
+            this.labelEncFiles.Name = "labelEncFiles";
+            this.labelEncFiles.Size = new System.Drawing.Size(88, 13);
+            this.labelEncFiles.TabIndex = 0;
+            this.labelEncFiles.Text = "Encrypted Files:";
+            // 
+            // listBoxEncFiles
+            // 
+            this.listBoxEncFiles.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxEncFiles.FormattingEnabled = true;
+            this.listBoxEncFiles.Location = new System.Drawing.Point(16, 25);
+            this.listBoxEncFiles.Name = "listBoxEncFiles";
+            this.listBoxEncFiles.Size = new System.Drawing.Size(468, 225);
+            this.listBoxEncFiles.TabIndex = 1;
+            // 
+            // labelLogs
+            // 
+            this.labelLogs.AutoSize = true;
+            this.labelLogs.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogs.Location = new System.Drawing.Point(13, 269);
+            this.labelLogs.Name = "labelLogs";
+            this.labelLogs.Size = new System.Drawing.Size(35, 13);
+            this.labelLogs.TabIndex = 2;
+            this.labelLogs.Text = "Logs:";
+            // 
+            // listBoxLogs
+            // 
+            this.listBoxLogs.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxLogs.FormattingEnabled = true;
+            this.listBoxLogs.Location = new System.Drawing.Point(16, 285);
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.Size = new System.Drawing.Size(468, 56);
+            this.listBoxLogs.TabIndex = 3;
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonScan.Location = new System.Drawing.Point(490, 25);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(146, 52);
+            this.buttonScan.TabIndex = 4;
+            this.buttonScan.Text = "Scan All Drives";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // buttonAddFile
+            // 
+            this.buttonAddFile.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddFile.Location = new System.Drawing.Point(566, 83);
+            this.buttonAddFile.Name = "buttonAddFile";
+            this.buttonAddFile.Size = new System.Drawing.Size(70, 52);
+            this.buttonAddFile.TabIndex = 5;
+            this.buttonAddFile.Text = "Add File";
+            this.buttonAddFile.UseVisualStyleBackColor = true;
+            this.buttonAddFile.Click += new System.EventHandler(this.buttonAddFile_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonQuit.Location = new System.Drawing.Point(566, 285);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(70, 56);
+            this.buttonQuit.TabIndex = 6;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(490, 198);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(146, 52);
+            this.buttonDecrypt.TabIndex = 7;
+            this.buttonDecrypt.Text = "Start Decryption";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // checkBoxKeep
+            // 
+            this.checkBoxKeep.AutoSize = true;
+            this.checkBoxKeep.Checked = true;
+            this.checkBoxKeep.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxKeep.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxKeep.Location = new System.Drawing.Point(490, 175);
+            this.checkBoxKeep.Name = "checkBoxKeep";
+            this.checkBoxKeep.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxKeep.TabIndex = 8;
+            this.checkBoxKeep.Text = "Keep Encrypted Files";
+            this.checkBoxKeep.UseVisualStyleBackColor = true;
+            this.checkBoxKeep.CheckStateChanged += new System.EventHandler(this.checkBoxKeep_CheckStateChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAbout.Location = new System.Drawing.Point(490, 285);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(70, 56);
+            this.buttonAbout.TabIndex = 9;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // buttonAddFolder
+            // 
+            this.buttonAddFolder.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddFolder.Location = new System.Drawing.Point(490, 83);
+            this.buttonAddFolder.Name = "buttonAddFolder";
+            this.buttonAddFolder.Size = new System.Drawing.Size(70, 52);
+            this.buttonAddFolder.TabIndex = 10;
+            this.buttonAddFolder.Text = "Scan Folder";
+            this.buttonAddFolder.UseVisualStyleBackColor = true;
+            this.buttonAddFolder.Click += new System.EventHandler(this.buttonAddFolder_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 350);
+            this.Controls.Add(this.buttonAddFolder);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.checkBoxKeep);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonAddFile);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.listBoxLogs);
+            this.Controls.Add(this.labelLogs);
+            this.Controls.Add(this.listBoxEncFiles);
+            this.Controls.Add(this.labelEncFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crypren Decrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelEncFiles;
+        private System.Windows.Forms.ListBox listBoxEncFiles;
+        private System.Windows.Forms.Label labelLogs;
+        private System.Windows.Forms.ListBox listBoxLogs;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.Button buttonAddFile;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.CheckBox checkBoxKeep;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonAddFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
